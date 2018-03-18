27fba718c63b14847a26f6f306ddae5237e9dcc3
ElinaDanilova <numlock777@yandex.ru>
2018-03-18 19:18:11 +0300

    update

--- src/main/java/ru/elina/PrintProduct.java
+++ src/main/java/ru/elina/PrintProduct.java
@@ -15,21 +15,32 @@ public class PrintProduct extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ArrayList<String> product = new ArrayList <String>();
-//        ArrayList<Product> productList = new ArrayList <Product>();
-//        Product pr1 =  new Product("Table", 1000);
-//        productList.add(pr1);
-        product.add("Table");
-        product.add("Sofa");
-        product.add("Chair");
-        product.add("TV");
-        product.add("Door");
-        product.add("Lamp");
-        PrintWriter printWriter = response.getWriter();
-        printWriter.write("<h1>Our products</h1>");
-        for (int i = 0; i < product.size(); i++)
-            printWriter.write("<table><tr><td><th>" + product.get(i) + "</th></td></tr></table>");
+//        ArrayList<String> product = new ArrayList <String>();
 
+
+        ArrayList<Product> productList = new ArrayList <Product>();
+        Product pr1 =  new Product("Table", 1000);
+        productList.add(pr1);
+        Product pr2 =  new Product("Sofa", 15000);
+        productList.add(pr2);
+        Product pr3 =  new Product("Chair", 1500);
+        productList.add(pr3);
+        Product pr4 =  new Product("TV", 40000);
+        productList.add(pr4);
+
+//        product.add("Table");
+//        product.add("Sofa");
+//        product.add("Chair");
+//        product.add("TV");
+//        product.add("Door");
+//        product.add("Lamp");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("<h1>Our products:</h1>");
+        printWriter.write("<table border bgcolor=\"#FFE4B5\" width=\"150\" height=\"50\"><tr><td align=\"center\" valign=\"middle\" width=\"75\" height=\"50\">Name</td><td align=\"center\" valign=\"middle\"  width=\"75\" height=\"50\">Price</td></tr></table>");
+//        for (int i = 0; i < product.size(); i++)
+//            printWriter.write("<table><tr><td><th>" + product.get(i) + "</th></td></tr></table>");
+        for (int i = 0; i < productList.size(); i++)
+            printWriter.write("<table border bgcolor=\"#FFF8DC\" width=\"150\" height=\"50\"><tr><td align=\"center\" valign=\"middle\" width=\"75\" height=\"50\">" + productList.get(i).name +"</td><td align=\"center\" valign=\"middle\" width=\"75\" height=\"50\">"+productList.get(i).price + "</td></tr></table>");
 
     }
 }
